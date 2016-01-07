3351d0e9d9329180d527c947a9f402e6c6815035
chrisbrasington <chrisbrasington@gmail.com>
2016-01-07 10:55:33 -0700

    ignore certain emails. loop indefinitely

--- check-email.rb
+++ check-email.rb
@@ -16,41 +16,57 @@ Gmail.connect!(Credentials.email, Credentials.password) do |gmail|
     if !gmail.logged_in?
         puts 'Failure to login'
     else
-        if gmail.inbox.count > 0
-            emails = gmail.inbox.emails(:unread, :from => Listener.phone)
-            print emails.count, " emails from: ", Listener.phone
-            puts
-            emails.each do |email|
-                email.message.attachments.each do |a|
-                    if File.extname(a.filename) == '.txt'
-                        text = a.body.to_s
-                        puts text
-                        if text == "What's up?"
-                            message = 'Feeling kinda bored ' #+ Time.now.to_s
-                            respond(gmail, Listener.phone, message)
-                            email.read!
-                            #email.archive! is currently broken, labeling as SMS
-                            email.move_to!("SMS");
-                        elsif text == 'weather'
-                            weather = %x[./weather.bat Denver]
-                            respond(gmail, Listener.phone, weather)
-                            email.read!
-                            #email.archive! is currently broken, labeling as SMS
-                            email.move_to!("SMS");
-                        elsif text.index('$') == 0
-                            command = 'transaction '
-                            command += "'" + text + "'"
-                            value = %x[#{command}]
-                            respond(gmail, Listener.phone, value)
-                            email.read!
-                            email.move_to!("SMS");
+        while true
+            if gmail.inbox.count > 0
+                emails = gmail.inbox.emails(:unread, :from => Listener.phone)
+                print emails.count, " emails from: ", Listener.phone
+                puts
+                emails.each do |email|
+                    email.message.attachments.each do |a|
+                        if File.extname(a.filename) == '.txt'
+                            text = a.body.to_s
+                            puts text
+                            if text == "What's up?"
+                                message = 'Feeling kinda bored ' #+ Time.now.to_s
+                                respond(gmail, Listener.phone, message)
+                                email.read
+                                #email.archive! is currently broken, labeling as SMS
+                                email.move_to("SMS")
+                            elsif text == 'weather'
+                                weather = %x[./weather.bat Denver]
+                                respond(gmail, Listener.phone, weather)
+                                email.read
+                                #email.archive! is currently broken, labeling as SMS
+                                email.move_to("SMS")
+                            elsif text.index('$') == 0
+                                command = 'transaction '
+                                command += "'" + text + "'"
+                                value = %x[#{command}]
+                                respond(gmail, Listener.phone, value)
+                                email.read
+                                email.move_to("SMS")
+                            else
+                                email.read
+                                email.move_to("Ignore")
+                            end
                         end
                     end
                 end
+                
+                emails = gmail.inbox.emails(:unread)
+                    if emails.count > 0
+                    print "ignoring ", emails.count, " other email(s)"
+                    puts
+                    emails.each do |email|
+                        email.read!
+                        email.move_to("Ignore")
+                    end
+                end
+            else
+                print "0 emails from: ", Listener.phone
+                puts
             end
-        else
-            print "0 emails from: ", Listener.phone
-            puts
+            sleep 10
         end
     end  
 end
