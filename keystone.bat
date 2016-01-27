072bca98fb791fe6aecf90a74bf67775bef9938a
chrisbrasington <chrisbrasington@gmail.com>
2016-01-26 19:03:33 -0700

    less AWK-ward piping. heh.

--- keystone.bat
+++ keystone.bat
@@ -1,1 +1,1 @@
-curl -s "http://www.keystoneresort.com/rss/snowreport.aspx"| awk 'gsub(/.*<description>|<\/description>.*/,"")' | awk 'gsub("&lt;br/&gt;", ". ") gsub("New Snow in last ", "")'
+curl -s "http://www.keystoneresort.com/rss/snowreport.aspx"| awk 'gsub(/.*<description>|<\/description>.*/,"") gsub("&lt;br/&gt;", ". ") gsub("New Snow in last ", "")'
