f71caf107cbc5f7cb6475ff8ea9937e7078a435a
chrisbrasington <chrisbrasington@gmail.com>
2016-04-23 20:28:33 -0600

    removing infinite loop, going to single run. ruby-gmail has some fucking deadlock detection that prevents the program from running as long as I would like. switching to use of a repetative cronjob

--- check-email.rb
+++ check-email.rb
@@ -16,114 +16,102 @@ def respond(gmail, email, message, subject_title)
     puts '----------------------------------'
 end
 
-def loop
-    # check every 10 seconds
-    counterSleep = 10
-    counterLoop = 0
-    
-    # logout/login to refresh session (and avoid timeout)
-    # every 1 minute (6 attempts)
-    sessionRefresh = 6
+def run
+    # check once, run again on cronjob
     
     # settings file
     settings = YAML.load_file('settings.yml')
 
     Gmail.connect!(settings['email'], settings['password']) do |gmail|
         if !gmail.logged_in?
-            puts 'Failure to login'
+            puts 'Failure to login.'
         else
-            puts 'Successful login'
-            print '  Check interval every ', counterSleep, ' seconds.'
-            puts
-            print '  Re-authenticate gmail session every ', sessionRefresh*counterSleep, ' seconds.'
-            puts
+            puts 'Successful login.'
+            puts 'Checking inbox...'
             
-            while counterLoop < sessionRefresh
-                print 'Checking inbox (', counterLoop+1, ')...'
+            # notify if nothing to do
+            foundMessage = false
+            
+            gmail.inbox.find(:unread, :from => settings['listener']).each do |email|
+                foundMessage = true
+                print 'Found email from ',settings['listener_name']
                 puts
-                counterLoop += 1
-                
-                gmail.inbox.find(:unread, :from => settings['listener']).each do |email|
-                    print 'Found email from ',settings['listener_name']
-                    puts
-                    email.message.attachments.each do |a|                        
-                        if File.extname(a.filename) == '.txt'
-                            text = a.body.to_s
-                            # avoid possible command line injection
-                            text = text.split('\'')[0]  
-                            if text.include? '\'' or text.include? ';' or text.include? '/'
+                email.message.attachments.each do |a|                        
+                    if File.extname(a.filename) == '.txt'
+                        text = a.body.to_s
+                        # avoid possible command line injection
+                        text = text.split('\'')[0]  
+                        if text.include? '\'' or text.include? ';' or text.include? '/'
+                            email.read!
+                            email.move_to("Ignore - Malicious")
+                        elsif
+                            if text == "status"
+                                puts 'Asked for status.'
+                                message = 'Feeling pretty great.' 
+                                respond(gmail, settings['listener'], message, 'Status Report')
                                 email.read!
-                                email.move_to("Ignore - Malicious")
-                            elsif
-                                if text == "status"
-                                    puts 'Asked for status.'
-                                    message = 'Feeling pretty great.' 
-                                    respond(gmail, settings['listener'], message, 'Status Report')
-                                    email.read!
-                                    #email.archive! is currently broken, labeling as SMS
-                                    email.move_to("SMS")
-                                elsif text.downcase == 'weather'
-                                    puts 'Running weather script..'
-                                    command = './weather.bat ' + settings['city']
-                                    weather = %x[#{command}]
-                                    respond(gmail, settings['listener'], weather, 'Weather')
-                                    email.read!
-                                    email.move_to("SMS")
-                                elsif text.downcase == 'snow' or text.downcase == 'keystone'
-                                    puts 'Running Keystone snow report..'
-                                    command = './keystone.bat'
-                                    snow = %x[#{command}]
-                                    respond(gmail, settings['listener'], snow, 'KeyStone Snow Report')
-                                    email.read!
-                                    email.move_to("SMS")
-                                elsif text.index('$') == 0
-                                    puts 'Running transaction script..'
-                                    # note transaction command is driven from another project
-                                    # to log financial tranasctions to gnucash
-                                    # https://github.com/chrisbrasington/text-messaging-to-gnucash
-                                    command = 'transaction '
-                                    command += "'" + text + "'"
-                                    value = %x[#{command}]
-                                    
-                                    puts command
-                                    puts
-                                    puts value
-                                    
-                                    respond(gmail, settings['listener'], value, 'Transaction')
-                                    email.read!
-                                    email.move_to("SMS")
-                                else
-                                    email.read!
-                                    email.move_to("Ignore")
-                                end
+                                #email.archive! is currently broken, labeling as SMS
+                                email.move_to("SMS")
+                            elsif text.downcase == 'weather'
+                                puts 'Running weather script..'
+                                command = './weather.bat ' + settings['city']
+                                weather = %x[#{command}]
+                                respond(gmail, settings['listener'], weather, 'Weather')
+                                email.read!
+                                email.move_to("SMS")
+                            elsif text.downcase == 'snow' or text.downcase == 'keystone'
+                                puts 'Running Keystone snow report..'
+                                command = './keystone.bat'
+                                snow = %x[#{command}]
+                                respond(gmail, settings['listener'], snow, 'KeyStone Snow Report')
+                                email.read!
+                                email.move_to("SMS")
+                            elsif text.index('$') == 0
+                                puts 'Running transaction script..'
+                                # note transaction command is driven from another project
+                                # to log financial tranasctions to gnucash
+                                # https://github.com/chrisbrasington/text-messaging-to-gnucash
+                                command = 'transaction '
+                                command += "'" + text + "'"
+                                value = %x[#{command}]
+                                
+                                puts command
+                                puts
+                                puts value
+                                
+                                respond(gmail, settings['listener'], value, 'Transaction')
+                                email.read!
+                                email.move_to("SMS")
+                            else
+                                email.read!
+                                email.move_to("Ignore")
                             end
                         end
                     end
-                                   
-                    gmail.inbox.find(:unread).each do |email|
-                        email.read!
-                        email.move_to("Ignore")
-                    end
                 end
-                sleep counterSleep
+                               
+                gmail.inbox.find(:unread).each do |email|
+                    email.read!
+                    email.move_to("Ignore")
+                end
             end
             
-            counterLoop = 0 # reset counter
+            if not foundMessage
+                puts "Nothing to do."
+            end
+            
             puts 'Logging out.'
             gmail.logout
         end  
     end
 end
 
-while true
-    begin
-        puts 'Beginning gmail listener.'
-        loop
-    rescue => exception
-        puts 'In rescue - sleeping for half a minute.'
-        puts exception
-        sleep 30
-    ensure
-        puts 'Restarting..'
-    end   
-end
+begin
+    puts 'Beginning gmail checker.'
+    run
+rescue => exception
+    puts 'In rescue - exiting.'
+    puts exception
+ensure
+    puts 'Exiting.'
+end   
